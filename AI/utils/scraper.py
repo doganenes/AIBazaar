5a0c7d91cad959fa3a387186ab1a1d7da138ccf2
Enes Doğan <enesdoganlive@hotmail.com>
2025-05-01 19:36:03 +0300

    update last scraper

--- AI/utils/scraper.py
+++ AI/utils/scraper.py
@@ -13,7 +13,72 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--window-size=1920,1080")
 global driver 
 
-product_lists = ["Xiaomi 13 256GB"]
+product_lists = [
+    "Motorola Fold",
+    "Google Pixel 9 Pro",
+    "Xiaomi 13 256GB",
+    "Iphone 15",
+
+    # Akıllı Telefonlar
+    "iPhone 16 Pro",
+    "Samsung Galaxy S24 Ultra",
+    "Huawei Mate XT Ultimate Design",
+    "vivo X200 Pro",
+    "HONOR Magic6 Pro",
+    "Xiaomi 14 Pro",
+    "OPPO Find X7 Pro",
+    "OnePlus 12",
+
+    # Dizüstü Bilgisayarlar
+    "Apple MacBook Pro 14 inç M3 Pro",
+    "ASUS Zenbook S14",
+    "ASUS Zenbook S16",
+    "Lenovo Şeffaf Ekranlı Laptop",
+    "Dell XPS 15 2024",
+    "HP Spectre x360 14",
+    "Razer Blade 16 2024",
+    "MSI Stealth 17 Studio",
+    "Acer Swift Edge 16",
+    "Samsung Galaxy Book4 Ultra",
+    "Microsoft Surface Laptop 6",
+
+    # Giyilebilir Teknolojiler
+    "Samsung Galaxy Ring",
+    "Apple Watch Ultra AI",
+    "Fitbit AI 2",
+    "Garmin AI Runners",
+    "Huawei Watch GT 4",
+    "Xiaomi Smart Band 8 Pro",
+    "OPPO Wearable Band",
+    "Withings ScanWatch 2",
+    "Amazfit Balance",
+    "Montblanc Summit 3",
+
+    # Sanal ve Artırılmış Gerçeklik
+    "Apple Vision XR",
+    "Oculus Quest 4 Pro",
+    "Sony PlayStation AR Kit",
+    "Meta Quest Vision",
+    "HTC Vive XR Elite",
+    "Lenovo ThinkReality A3",
+    "Magic Leap 2",
+    "Nreal Air 2",
+    "Pico 4 Enterprise",
+    "Xiaomi Wireless AR Glass Discovery Edition",
+
+    # Ses Aksesuarları
+    "ANKER Space One Bluetooth Kulaklık",
+    "Sennheiser Momentum 4 True Wireless",
+    "JBL Tour Pro 2",
+    "Sony WF-1000XM5",
+    "Bose QuietComfort Ultra Earbuds",
+    "Apple AirPods Pro 2",
+    "Samsung Galaxy Buds 3 Pro",
+    "Beats Studio Buds+",
+    "Nothing Ear (2)",
+    "Google Pixel Buds Pro"
+]
+
 
 
 def scraping_prices(product_list):
@@ -31,12 +96,14 @@ def scraping_prices(product_list):
             EC.element_to_be_clickable((By.CSS_SELECTOR, "#APL li img"))
         )
 
+        try:
         # ActionChains ile tıkla
-        actions = ActionChains(driver)
-        actions.move_to_element(first_product_link).click().perform()
-        canvas = wait.until(EC.presence_of_element_located((By.ID, "PG_C")))
-        tooltip = wait.until(EC.presence_of_element_located((By.ID, "tooltip")))
-
+            actions = ActionChains(driver)
+            actions.move_to_element(first_product_link).click().perform()
+            canvas = wait.until(EC.presence_of_element_located((By.ID, "PG_C")))
+            tooltip = wait.until(EC.presence_of_element_located((By.ID, "tooltip")))
+        except:
+            continue
         # Ürün adını al
         try:
             product_name = driver.find_element(By.TAG_NAME, "h1").text.strip()
@@ -62,6 +129,7 @@ def scraping_prices(product_list):
             element = driver.find_element(By.XPATH, "//label[@data-len='365']")
             driver.execute_script("arguments[0].click();", element)
         except:
+            pass
             print("Grafik gösterimi değiştirilemedi")
 
         for x in range(start_x, end_x, step):
@@ -86,7 +154,7 @@ def scraping_prices(product_list):
     driver.quit()
 
     # CSV'ye yaz
-    with open("akakce.csv", mode="w", newline="", encoding="utf-8-sig") as file:
+    with open("./csv/akakce.csv", mode="w", newline="", encoding="utf-8-sig") as file:
         writer = csv.writer(file)
         writer.writerow(["Product Name", "Date", "Price"])
         writer.writerows(all_data)
@@ -116,6 +184,7 @@ def price_runner(product_list):
         search_input.clear()
         search_input.send_keys(product_name)
         search_input.send_keys(Keys.ENTER)
+        search_input.clear()
         print("Arama yapıldı.")
 
         first_product = wait.until(
@@ -138,15 +207,39 @@ def price_runner(product_list):
             )
             time.sleep(1)
             popularity_button.click()
-        except Exception as e:
-            popularity_button = driver.find_element(
-                By.CSS_SELECTOR, ".pr-wyywe-Tabs-item"
-            )
-            if popularity_button.is_displayed():
-                actions = ActionChains(driver)
-                actions.move_to_element(popularity_button).click().perform()
-            print(f"Popülerlik butonuna tıklanamadı: {e}")
-
+        except:
+            try:
+                driver.execute_script("window.scrollTo(0, 0);")
+                # Butonu bul
+                price_history_button = driver.find_element(
+                    By.XPATH, '//*[@id="product-listing-navigation"]/div/div/button[3]'
+                )
+                price_history_button.click()
+                popularity_button = wait.until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH, '//*[@id="pricegraph"]/div[3]/div/button[2]')
+                    )
+                )
+                popularity_button.click()
+                print("Price history butonuna tıklandı.")
+            except:
+                try:
+                    driver.execute_script("arguments[0].click();", price_history_button)
+                    popularity_button = wait.until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH, '//*[@id="pricegraph"]/div[3]/div/button[2]')
+                    )
+                )
+                    popularity_button.click()
+                except:
+                    actions = ActionChains(driver)
+                    actions.move_to_element(price_history_button).click().perform()
+                popularity_button = wait.until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH, '//*[@id="pricegraph"]/div[3]/div/button[2]')
+                    )
+                )
+                popularity_button.click()
         try:
             month_element = wait.until(
                 EC.element_to_be_clickable(
@@ -160,10 +253,11 @@ def price_runner(product_list):
             print(f"12 ay hatası: {e}")
 
         time.sleep(3)
-
+    try:
         chart = driver.find_element(By.CLASS_NAME, "highcharts-series")
         actions = ActionChains(driver)
-
+    except:
+        pass
         start_x = -100
         end_x = 700
         step = 30
@@ -188,7 +282,7 @@ def price_runner(product_list):
         driver.get("https://www.pricerunner.com")
     driver.quit()
 
-    with open("price_runner.csv", mode="w", newline="", encoding="utf-8") as file:
+    with open("./csv/price_runner.csv", mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerow(["Product Name ", "Popularity"])
         writer.writerows(tooltip_popularities)
@@ -268,19 +362,14 @@ def scraping_description_and_image(product_list):
                             By.CSS_SELECTOR, ".attr-value-name-w"
                         ).text.strip()
                         product_details[key] = value
+                        key_value_text = ' '.join(f"{key}:{value};" for key, value in product_details.items())
+                        print(key_value_text)
                     except:
                         continue
+                results.append((key_value_text,product_name,image_src,rating_value))
+
             except:
                 pass
-
-            results.append(
-                {
-                    "Product Name": product_name,
-                    "Image": image_src,
-                    "Description": product_details,
-                    "Rating": rating_value,
-                }
-            )
 
             # Ürün sekmesini kapat ve ana sekmeye dön
             driver.close()
@@ -292,10 +381,10 @@ def scraping_description_and_image(product_list):
             continue
 
     driver.quit()
-    with open("trendyol.csv", mode="w", newline="", encoding="utf-8-sig") as file:
+    with open("./csv/trendyol.csv", mode="w", newline="", encoding="utf-8-sig") as file:
         writer = csv.writer(file)
-        writer.writerow(["Product Name ", "Image" , "Description" , "Rating"])
-        writer.writerows(product_details)
+        writer.writerow(["Description", "Product Name", "Image", "Rating"])
+        writer.writerows(results)
 
 
 
@@ -311,9 +400,9 @@ threads = []
 # Fonksiyonları thread'lere referans olarak ekleyip çalıştırıyoruz
 threads.append(threading.Thread(target=scraping_prices, args=(product_lists,)))
 threads.append(threading.Thread(target=price_runner, args=(product_lists,)))
-threads.append(
-    threading.Thread(target=scraping_description_and_image, args=(product_lists,))
-)
+# threads.append(
+#     threading.Thread(target=scraping_description_and_image, args=(product_lists,))
+# )
 
 # Thread'leri başlat
 for t in threads:
