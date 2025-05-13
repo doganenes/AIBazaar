f9af04ef62f63f5364851903021d0a29b55b20b8
Enes Doğan <enesdoganlive@hotmail.com>
2025-05-13 14:40:34 +0300

    update

--- Frontend/api/api.js
+++ Frontend/api/api.js
@@ -9,3 +9,26 @@ const api = axios.create({
   },
 });
 
+export const signup = async (formData) => {
+  try {
+    const response = await api.post("/api/auth/signup", formData);
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    }
+    throw new Error("Network error occurred.");
+  }
+};
+
+export const login = async (formData) => {
+  try {
+    const response = await api.post("/api/auth/login", formData);
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    }
+    throw new Error("Network error occurred.");
+  }
+};
