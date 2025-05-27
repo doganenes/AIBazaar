817af3655379ac39e1ca3574b42b57d3bb40c175
Enes Doğan <enesdoganlive@hotmail.com>
2025-05-27 13:00:50 +0300

    fix

--- Frontend/pages/Home.jsx
+++ Frontend/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
 import { getAllProducts } from "../api/api";
+import "../css/Home.css";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -18,8 +19,8 @@ const Home = () => {
   }, [navigate]);
 
   return (
-    <div className="container-fluid">
-      <div className="row">
+    <div className="container-fluid homeContainer">
+      <div className="row mt-5">
         {products.map((product) => (
           <div className="col-md-3 mt-3" key={product.productID}>
             <ProductCard product={product} />
